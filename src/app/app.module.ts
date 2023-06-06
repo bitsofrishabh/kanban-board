c02def9a655381571f70cbe630162cb13bc3b98f
Rishabh.Singh12 <rishabh_singh2@apple.com>
2023-06-06 16:53:12 +0530

    firestore integrated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from './shared/shared.module';
+import { HomePageComponent } from './home-page/home-page.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, HomePageComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    SharedModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: true,
+    firebase: {
+      projectId: 'kanban-board-b1d5f',
+      appId: '1:154806555921:web:659a62b83f5fa9841d28b0',
+      storageBucket: 'kanban-board-b1d5f.appspot.com',
+      apiKey: 'AIzaSyADVXM6eYPHa_nHey8-W6TT_2OH4q8s0tw',
+      authDomain: 'kanban-board-b1d5f.firebaseapp.com',
+      messagingSenderId: '154806555921',
+      measurementId: 'G-0Q8QP7KYD6',
+    },
+  };
